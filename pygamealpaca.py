6c33f575a09f9281e20adaf7a6192e852d377a7e
AlpacaPlayz <aspenekey@yahoo.com>
2016-10-12 17:47:52 -0400

    Add files via upload

---
+++ pygamealpaca.py
@@ -0,0 +1,26 @@
+print("Welcome to this RANDOMNESS QUIZ!")
+name = input("What is your name?")
+print("Hello, " + name)
+
+
+print("Let's start with the easiest question: What is 1+1?")
+answer = input()
+if answer == "2":
+    print("Obviously.")
+else:
+    print("VERY incorrect.")
+
+print("True or False, is Pikachu a Pokemon?")
+answer = input()
+if answer == "true":
+    print("That one was easy.")
+else:
+    print("NOPE.")
+
+print("One last easy one, what is Percy Jackson's sword?")
+answer = input()
+if answer == "Riptide":
+    print("Yassss.")
+else:
+    print("That is 100% not correct.")
+    
