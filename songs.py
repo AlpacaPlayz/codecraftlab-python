3f1a531d37b579f6f32ddb099e6d6e7113338ea4
AlpacaPlayz <aspenekey@yahoo.com>
2017-01-05 14:05:36 -0500

    Add files via upload

---
+++ songs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import pygame
+pygame.init()
+song = pygame.mixer.Sound('thesong.ogg')
+clock = pygame.time.Clock()
+song.play()
+while True:
+    clock.tick(60)
+pygame.quit()
